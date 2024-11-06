0b3ae866158832179607da9ff802a6e62a44ca74
Zakaria BARI <zakariabari213@gmail.com>
2024-11-06 16:15:18 +0100

    Update

--- JS/main.js
+++ JS/main.js
@@ -96,21 +96,80 @@ function sendMail(event) {
   event.preventDefault(); // Prevent the default form submission
 
   let parms = {
+    
     name: document.getElementById("name").value,
     email: document.getElementById("email").value,
     subject: document.getElementById("subject").value,
     message: document.getElementById("message").value,
-  };
 
-  emailjs
-    .send("service_mb5u1nl", "template_gwg8zyn", parms)
-    .then(function () {
-      alertify.alert("Email sent successfully!");
-      document.getElementById("form").reset();
-    })
-    .catch(function (error) {
-      console.error("Error sending email:", error);
-    });
+  };
+  
+  let messageError = document.getElementsByClassName('messageError');
+
+  var nameRegex = new RegExp(/^[a-z]+[A-Z]?$/);
+  let checkName = parms.name;
+  var validName = nameRegex.test(checkName);
+  if (checkName == "") {
+      messageError[0].innerHTML = "Name Required*";
+      messageError[0].style.color = "red";
+      messageError[0].style.fontWeight = "bold";
+  }
+  else if (validName == false){
+      messageError[0].innerHTML = "Digits and symbol not included*";
+      messageError[0].style.color = "red";
+      messageError[0].style.fontWeight = "bold";
+  }
+  var emailRegex = new RegExp(/^[a-z\d.]+@[a-z\d]+.([a-z]{2,8})(.[a-z]{2,8})?$/);
+  let checkEmail = parms.email;
+  var validEmail = emailRegex.test(checkEmail);
+  if (checkEmail == "") {
+      messageError[1].innerHTML = "Email Required*";
+      messageError[1].style.color = "red";
+      messageError[1].style.fontWeight = "bold";
+  }
+  else if (validEmail == false){
+      messageError[1].innerHTML = "Email should be like : example@contact.com*";
+      messageError[1].style.color = "red";
+      messageError[1].style.fontWeight = "bold";
+  }
+
+  var subjetRegex = new RegExp(/^[a-z.,][A-Z.,]?$/);
+  let checkSubject = parms.subject;
+  var validSubject = subjetRegex.test(checkSubject);
+  if (checkSubject == "") {
+      messageError[2].innerHTML = "Subject Required*";
+      messageError[2].style.color = "red";
+      messageError[2].style.fontWeight = "bold";
+  }
+  else if (validSubject == false){
+      messageError[2].innerHTML = "Subject invalid*";
+      messageError[2].style.color = "red";
+      messageError[2].style.fontWeight = "bold";
+  }
+
+  var messageRegex = new RegExp(/^[a-z\d.,][A-Z.,][0-9]?$/);
+  let checkMessage = parms.message;
+  var validMessage = messageRegex.test(checkSubject);
+  if (checkMessage == "") {
+      messageError[3].innerHTML = "Subject Required*";
+      messageError[3].style.color = "red";
+      messageError[3].style.fontWeight = "bold";
+  }
+  else if (validMessage == false){
+      messageError[3].innerHTML = "Subject invalid*";
+      messageError[3].style.color = "red";
+      messageError[3].style.fontWeight = "bold";
+  }
+  else {
+    emailjs.send("service_mb5u1nl", "template_gwg8zyn", parms)
+      .then(function () {
+        alertify.alert("Email sent successfully!");
+        document.getElementById("form").reset();
+      })
+      .catch(function (error) {
+        console.error("Error sending email:", error);
+      });
+  }
 }
 
 //  ~~~~~~~~~~~~~~~ END CONTACT ~~~~~~~~~~~~~~~
